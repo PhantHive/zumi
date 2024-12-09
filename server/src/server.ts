bb59afc9db23fe576045cf6e9a74505e2bbf328d
PhantHive <phantomhive72@yahoo.fr>
2024-12-09 15:55:30 +0100

    feat: oauth google + refractor from commonjs  to esm

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,38 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import songRoutes from './routes/songs';
+import { fileURLToPath } from 'url';
+import songRoutes from './routes/songs.js';
+import connectDB from './utils/mongoose.js';
+import dotenv from 'dotenv';
+import { auth } from './middlewares/auth.js';
+
+// Load environment variables
+dotenv.config();
+
+// Connect to the database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
+
+// Authentication middleware
+app.use(auth);
+
+// Routes
 app.use('/api/songs', songRoutes);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Export default for ESNext compatibility
+export default app;
